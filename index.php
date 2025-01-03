3ae3750948548f6ee043b7d1456ca9089c874d07
Nirqu <BeChilliN369@gmail.com>
2025-01-03 20:39:26 +0300

    feat: add task creation form with basic styling and layout improvements

--- index.php
+++ index.php
@@ -8,5 +8,28 @@
 </head>
 <body>
 <h1>Welcome to Task Manager</h1>
+
+
+<form action="" method="post">
+    <p>
+        <label for="name">Task name</label>
+        <input type="text" id="name" name="task_name"/>
+    </p>
+        <label for="description"></label>
+        <textarea rows="5" cols="33" id="description" name="task_description" placeholder="Task description here..."></textarea>
+    <p>
+        <label for="prior">Priority</label>
+        <select name="task_prior" id="prior">
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+        </select>
+    </p>
+    <p>
+        <input type="submit"/>
+    </p>
+</form>
+
+
 </body>
 </html>
