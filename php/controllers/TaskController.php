cf4114ea5dd09f8cd3ca50412941765fdc35ccc6
Nirqu <BeChilliN369@gmail.com>
2025-01-10 22:56:05 +0300

    feat: start implementing task name uniqueness check

--- php/controllers/TaskController.php
+++ php/controllers/TaskController.php
@@ -1,7 +1,19 @@
 <?php
 require 'get_task_list.php';
 
+function checkInArray() {
+    $tasks = getTasksData();
+    foreach ($tasks as $task) {
+        if($tasks['Name'] === $_POST['name']){
+            die('Fatal Error');
+        }
+    }
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
     require_once 'add_task.php';
     $task_data = array();
     $task_data[] = array(
@@ -9,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'Description' => $_POST['description'],
         'Priority' => $_POST['priority'],
     );
+    checkInArray($tasks_data['Name']);
     add_task($task_data);
 }
 
